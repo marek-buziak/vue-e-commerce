150ff7546964f22715ccc659984f3e6d056d3a42
Marek Buziak <marek.buziak@gmail.com>
2022-10-25 15:04:05 +0200

    [3] Added loading spinner component and passed data to ProductBox

---
+++ src/components/ProductBox.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const props = defineProps({
+  id: Number,
+  title: String,
+  price: Number,
+  category: String,
+  image: String,
+  description: String
+})
+</script>
+
+<template>
+	<div class="product-box">
+		<div class="product-image">
+            <img :src="props.image" alt="">
+        </div>
+		<div class="product-category">{{props.category}}</div>
+		<div class="product-title">{{props.title}}</div>
+		<div class="product-description">{{props.description}}</div>
+		<div class="actions">
+			<div class="product-price">{{props.price}}</div>
+			<div class="add-to-cart">
+				<button>Add</button>
+			</div>
+		</div>
+	</div>
+</template>
--- src/components/ProductsList.vue
+++ src/components/ProductsList.vue
@@ -1,14 +1,16 @@
 <script setup lang="ts">
 import { ref, onMounted } from "vue";
 import axios from "axios";
+import ProductBox from "@/components/ProductBox.vue";
+import LoadingSpinner from "@/components/LoadingSpinner.vue";
 
-let productsData = ref(null);
+let productsData = ref<any[]>([]);
 console.log("productsData - initial:", productsData.value);
 
 const getProductsData = async () => {
     console.log("getProductsData");
 	try {
-		const response = await axios.get("https://api.escuelajs.co/api/v1/products");
+		const response = await axios.get("https://fakestoreapi.com/products");
 		console.log("response:", response);
         productsData.value = response.data;
         console.log("productsData - updated:", productsData.value);
@@ -24,10 +26,8 @@ onMounted(() => {
 
 <template>
 	<h1>ProductList component</h1>
-    <h2 v-if="productsData">
-        Products loaded
-    </h2>
-    <h2 v-else>
-        Loading...
-    </h2>
+    <section class="products" v-if="productsData.length">
+        <ProductBox v-for="product in productsData" :key="product.id" v-bind="product"/>
+    </section>
+    <LoadingSpinner v-else/>
 </template>
