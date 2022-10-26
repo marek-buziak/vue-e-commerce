a3bd658562cc266fdf2cf2d70132ba096a711e4f
Marek Buziak <marek.buziak@gmail.com>
2022-10-26 21:20:20 +0200

    [4]

--- src/components/ProductBox.vue
+++ src/components/ProductBox.vue
@@ -5,18 +5,19 @@ const props = defineProps({
   price: Number,
   category: String,
   image: String,
-  description: String
+  description: String,
+  rating: Object
 })
 </script>
 
 <template>
-	<div class="product-box">
-		<div class="product-image">
-            <img :src="props.image" alt="">
+	<div class="product-box flex flex-col items-center bg-white p-4 rounded-3xl">
+		<div class="product-image flex justify-center h-56 w-56">
+            <img class="h-full object-contain rounded-3xl" :src="props.image" :alt="props.title">
         </div>
 		<div class="product-category">{{props.category}}</div>
-		<div class="product-title">{{props.title}}</div>
-		<div class="product-description">{{props.description}}</div>
+		<div class="product-title truncate w-full">{{props.title}}</div>
+		<div class="product-description truncate w-full">{{props.description}}</div>
 		<div class="actions">
 			<div class="product-price">{{props.price}}</div>
 			<div class="add-to-cart">
@@ -25,3 +26,19 @@ const props = defineProps({
 		</div>
 	</div>
 </template>
+<style>
+
+    .product-image {
+        margin-top: -48px;
+    }
+    /* .product-box > * {
+        width: 100%;
+    }
+    .product-image {
+        width: 200px;
+    }
+    .product-image img {
+        width: 100%;
+        height: auto;
+    } */
+</style>
--- src/components/ProductsList.vue
+++ src/components/ProductsList.vue
@@ -26,8 +26,40 @@ onMounted(() => {
 
 <template>
 	<h1>ProductList component</h1>
-    <section class="products" v-if="productsData.length">
+    <section class="products md:container mx-auto px-2 sm:px-4 sm:flex sm:flex-wrap" v-if="productsData.length">
         <ProductBox v-for="product in productsData" :key="product.id" v-bind="product"/>
     </section>
     <LoadingSpinner v-else/>
 </template>
+<style>
+
+    .product-box {
+        margin-bottom: 4rem;
+        width: 100%;
+    }
+
+    @media (min-width: 640px) {
+        .product-box {
+            width: calc(100% / 2 - 1rem);
+        }
+
+        .product-box:nth-child(2n+1) {
+            margin-right: 2rem;
+        }
+    }
+
+    @media (min-width: 1024px) {
+        .product-box {
+            width: calc(100% / 3 - 1rem);
+        }
+
+        .product-box:nth-child(2n+1) {
+            margin-right: unset;
+        }
+
+        .product-box:nth-child(3n+1),
+        .product-box:nth-child(3n+2) {
+            margin-right: 1.5rem;
+        }
+    }
+</style>
